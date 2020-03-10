59c53d76a0961967175097a4d742319dec73a9b6
Taj485 <tajbi21@gmail.com>
2020-03-10 00:25:08 +0000

    added more posts

--- src/app/message-board/message-board.component.ts
+++ src/app/message-board/message-board.component.ts
@@ -5,10 +5,9 @@ import { Component, OnInit } from '@angular/core';
   template: `
   <div>
     <h1>Messages Board</h1>
-    <hr/>
     <post-thumbnail 
       (eventClick)="handleChildEvent($event)"
-      [messages]="posts">
+      *ngFor="let post of posts" [messages]="post">
     </post-thumbnail>
   </div>
   `,
@@ -21,13 +20,26 @@ export class MessageBoardComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  posts = {
-    id: 1,
-    name:'Donnie Yen',
-    date: '01/03/2020',
-    post: 'My first post!',
-    imageUrl: '/assets/images/donnie yen.jpg'
-  };
+  posts = [
+    { id: 1,
+      name:'Jenifer',
+      date: '01/03/2020',
+      post: 'My first post!',
+      imageUrl: 'app/assets/images/happy.jpeg'
+    },
+    { id: 2,
+      name:'Donnie Yen',
+      date:'09/03/2020',
+      post:'Have you seen my latest movie IP Man 4?',
+      imageUrl:'app/assets/images/donnie yen.jpg'
+    },
+    { id: 3,
+      name:'Will Smith',
+      date:'01/03/2020',
+      post:'Now this is a story all about how My life got flipped-turned upside down',
+      imageUrl:'app/assets/images/will smith.jpg'
+    }
+  ];
 
   handleChildEvent(data) {
      console.log(data)
